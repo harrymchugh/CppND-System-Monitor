5189bc34ed144b9cd165e61cc8cbaab9f6538f2e
harry <harrymchugh@gmail.com>
2021-07-06 13:57:22 +0100

    Fix format.cpp

--- src/format.cpp
+++ src/format.cpp
@@ -1,6 +1,8 @@
 #include <string>
-
+#include <stdio.h>
 #include "format.h"
+
+#include <iostream>
 
 using std::string;
 
@@ -8,4 +10,13 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+    char buffer [15];
+    int mins, hours, newsecs;
+    hours = (seconds / 60 / 60);
+    mins = (seconds / 60) - (hours * 60);
+    newsecs = seconds - (hours * 60 * 60) - (mins * 60);
+    std::snprintf(buffer, sizeof(buffer),"%02d:%02d:%02d",hours,mins,newsecs);
+    std::string output = buffer;    
+    return output; 
+}
